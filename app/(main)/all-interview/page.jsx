68ce3e6bbe0eb77056bf56f58cc11d05f48c0d75
cygon23 <rweyemamu088@gmail.com>
2025-06-10 23:10:15 +0300

    list all interview complete

---
+++ app/(main)/all-interview/page.jsx
@@ -0,0 +1,53 @@
+"use client";
+import { useUser } from "@/app/provider";
+import { Button } from "@/components/ui/button";
+import { supabase } from "@/services/superbaseClient";
+import { Camera, Video } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+import InterviewCard from "../dashboard/_components/InterviewCard";
+
+function AllInterview() {
+  const [InterviewsList, setInterviewsList] = useState([]);
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && GetInterviewList();
+  }, [user]);
+
+  const GetInterviewList = async () => {
+    let { data: interviews, error } = await supabase
+      .from("interviews")
+      .select("*")
+      .eq("userEmail", user?.email)
+      .order("id", { ascending: false });
+
+    // console.log(interviews);
+
+    setInterviewsList(interviews);
+  };
+
+  return (
+    <div className='my-5'>
+      <h2 className='font-bold text-2xl'> All Previously Created Intervews</h2>
+
+      {InterviewsList?.length == 0 && (
+        <div className='p-5 flex flex-col gap-3 items-center  mt-5'>
+          <Video className='h-10 w-10 text-primary' />
+          <h2>You don't have any interview created?</h2>
+          <Button>+ Create New</Button>
+        </div>
+      )}
+
+      {InterviewsList && (
+        <div className='grid grid-cols-2 mt-5 xl:grid-cols-3 gap-5 '>
+          {InterviewsList.map((interviews, index) => (
+            <InterviewCard interviews={interviews} key={index} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AllInterview;
